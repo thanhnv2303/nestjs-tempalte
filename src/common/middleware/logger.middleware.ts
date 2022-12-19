8059b86758c9b499f7d9e31c1fc9907a043c2aec
linlin <nguyenthanh2303@gmail.com>
2022-12-19 11:27:42 +0700

    feat: setup logger

---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,34 @@
+import { HttpStatus, Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+import { Logger } from "@nestjs/common/services/logger.service";
+
+const logger = new Logger();
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const startTime = new Date().getTime();
+    try {
+      console.log("hello");
+      next();
+      const responseStatus = res.statusCode;
+      logger.log(`${getTimeDelta(startTime)}ms ${req.ip} ${responseStatus} ${req.method} ${getUrl(req)}`);
+
+    } catch (e) {
+      logger.error(`${getTimeDelta(startTime)}ms ${req.ip} ${e.status} ${req.method} ${getUrl(req)}`);
+      logger.error(e);
+    }
+
+  }
+
+
+}
+
+function getTimeDelta(startTime: number): number {
+  return new Date().getTime() - startTime;
+}
+
+function getUrl(request: Request): string {
+  return `${request.protocol}://${request.get("host")}${request.originalUrl}`;
+}
