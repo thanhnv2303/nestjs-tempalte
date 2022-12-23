b6096d0a3fbcf13dffcd7732988cf06d20baae05
linlin <nguyenthanh2303@gmail.com>
2022-12-23 16:39:25 +0700

    fix: update add API_PREFIX

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import { createSwagger } from "./common/setup/swagger";
-
+const API_DEFAULT_PREFIX = '/';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   createSwagger(app);
+  app.setGlobalPrefix(process.env.API_PREFIX || API_DEFAULT_PREFIX);
 
   app.use(cookieParser());
 
