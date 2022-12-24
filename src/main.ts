1767a83d82197f0a27b3bcf0e36c6c6b3cd6e9ea
linlin <nguyenthanh2303@gmail.com>
2022-12-24 10:25:07 +0700

    feat: update https

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,27 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import { createSwagger } from "./common/setup/swagger";
-const API_DEFAULT_PREFIX = '/';
+import * as fs from "fs";
+
+const API_DEFAULT_PREFIX = "/";
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  let httpsOptions;
+  if (!process.env.HTTPS_ENABLE || process.env.HTTPS_ENABLE === "1") {
+    httpsOptions = {
+      key: fs.readFileSync("./secrets/private-key.pem"),
+      cert: fs.readFileSync("./secrets/public-certificate.pem")
+    };
+  }
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   createSwagger(app);
   app.setGlobalPrefix(process.env.API_PREFIX || API_DEFAULT_PREFIX);
 
   app.use(cookieParser());
 
-  await app.listen(process.env.PORT || 3000,process.env.HOST || "0.0.0.0");
+  await app.listen(process.env.PORT || 3000, process.env.HOST || "0.0.0.0");
 
 }
 
