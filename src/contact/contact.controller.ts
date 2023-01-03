542eccb74132c11aee58638b9e2491364827ecbd
linlin <nguyenthanh2303@gmail.com>
2023-01-03 17:22:28 +0700

    fix: update response des for contact

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, Response } from "@nestjs/common";
 import { ContactService } from "./contact.service";
 import { UpsetContactDto } from "./dto/upset-contact.dto";
 import isEmail from "validator/lib/isEmail";
+import { ApiBadRequestResponse, ApiOkResponse } from "@nestjs/swagger";
 
 @Controller("contact")
 export class ContactController {
@@ -9,9 +10,11 @@ export class ContactController {
   }
 
   @Post()
+  @ApiBadRequestResponse({ status: 400, description: "Invalid email " })
+  @ApiOkResponse({ status: 200, description: "Submit success." })
   async create(@Body() upsetContactDto: UpsetContactDto, @Response() res) {
     if (!isEmail(upsetContactDto.email)) {
-      return res.status(406).send(`Invalid email ${upsetContactDto.email}`);
+      return res.status(400).send(`Invalid email "${upsetContactDto.email}"`);
     }
 
     for (const upsetContactDtoElement of upsetContactDto.enquiries) {
